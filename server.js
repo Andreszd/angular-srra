d64268cdd3bdb02019bffff38f9c937536a81053
jose andres <contretaa@gmail.com>
2020-11-13 23:09:00 -0400

    fix server.js

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.use(express.static(__dirname+'/dist/SraaClient'));
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/SraaClient/index.html'));
 });
-app.set("port", process.env || 3000)
+app.set("port", process.env.PORT || 3000)
 
 app.listen(app.get("port"))
